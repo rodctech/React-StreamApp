841b7c633f76ef2c6ce3f0a98751837047e19172
Rodrigo Chavez <rodctech@gmail.com>
2019-05-22 15:23:30 -0500

    Initial Stream App Commit

---
+++ client/src/components/streams/StreamShow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StreamShow = () => {
+    return (
+        <div>
+            Steam Show
+        </div>
+    );
+};
+
+export default StreamShow;
---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+//Push to GitHub test
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App';
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
