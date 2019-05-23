07d44b9188e1b8731daff74e2b35c36f2bf1dfaa
Rodrigo Chavez <rodctech@gmail.com>
2019-05-23 12:34:54 -0500

    L229 . in StreamCreate.js Hooked up Field Components  to new renderInput()   NEW Syntax, when field calls renderInput it passes properties taken and passed to txt input on screen

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,41 @@
+import React, { Component } from "react";
+import {Field, reduxForm } from "redux-form";
+//Field is UpperCase since React Component that will show on screen.
+// reduxForm is a function same functionality as the connect() from react-redux
+
+class StreamCreate extends Component {
+    renderInput({input}) {  //  destructured: formProps){
+       // console.log(formProps);
+        return ( <input {...input} /> // destructured: formProps.input} />
+        /*  <input
+            onChange={formProps.input.onChange}
+            value={formProps.input.value}
+          />*/
+        );
+    }
+
+    render() {
+        return (
+          <form>
+              <Field name={"title"} component={this.renderInput}/>
+              <Field name={"description"} component={this.renderInput}/>
+          </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'streamCreateTemp'
+})(StreamCreate);
+
+/*
 import React from "react";
+
+
 
 const StreamCreate = () => {
     return <div>Stream Create</div>;
 };
 
 export default StreamCreate;
+*/
