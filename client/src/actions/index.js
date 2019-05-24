ae6b9d979c1dc8e97ce043f4abe7c6b24ec18829
Rodrigo Chavez <rodctech@gmail.com>
2019-05-24 12:56:54 -0500

    L257   -Added Programmatic Navigation  after Create Stream button takes us back to main path '/'

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -31,6 +32,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
   //Do Some Programmatic Navigation to get user back to the root route
+  history.push('/');
 
 };
 
