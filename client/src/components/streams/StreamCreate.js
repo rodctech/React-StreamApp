8395020a8941bb48f6e76d78f7393afd3a77706d
Rodrigo Chavez <rodctech@gmail.com>
2019-05-24 18:34:25 -0500

    L266  -Refactored SCreate file  Refactored StreamCreate to use the new StreamForm file

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,58 +1,62 @@
 import React, { Component } from "react";
-import { Field, reduxForm } from "redux-form";
+import StreamForm from "./StreamForm";
 import { connect, create } from "react-redux";
-import {createStream} from "../../actions";
+import { createStream } from "../../actions";
 //Field is UpperCase since React Component that will show on screen.
 // reduxForm is a function same functionality as the connect() from react-redux
 
 class StreamCreate extends Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className={"ui error message"}>
-          <div className={"header"}>{error}</div>
-        </div>
+  /* renderError({ error, touched }) {
+     if (touched && error) {
+       return (
+         <div className={"ui error message"}>
+           <div className={"header"}>{error}</div>
+         </div>
+       );
+     }
+   }*/
+
+  /*  renderInput = ({ input, label, meta }) => {  //  destructured: formProps){
+      //console.log(meta);
+      // console.log(formProps);
+      const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+      return (    // destructured: formProps.input} />
+        // autoComplete turns off AutoFill dropDown
+        <div className={className}>
+          <label>{label} </label>
+          <input {...input} autoComplete={"off"}/>
+          {this.renderError(meta)}
+        </div>      //<div>{meta.error}</div>
+        /!*  <input
+            onChange={formProps.input.onChange}
+            value={formProps.input.value}
+          />*!/
       );
-    }
-  }
+    };*/
 
-  renderInput = ({ input, label, meta }) => {  //  destructured: formProps){
-    //console.log(meta);
-    // console.log(formProps);
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    return (    // destructured: formProps.input} />
-      // autoComplete turns off AutoFill dropDown
-      <div className={className}>
-        <label>{label} </label>
-        <input {...input} autoComplete={"off"}/>
-        {this.renderError(meta)}
-      </div>      //<div>{meta.error}</div>
-      /*  <input
-          onChange={formProps.input.onChange}
-          value={formProps.input.value}
-        />*/
-    );
-  };
-
-  onSubmit = (formValues)  => {
+  onSubmit = (formValues) => {
     this.props.createStream(formValues);
     // console.log(formValues);
     //event.preventDefault();
-  }
+  };
 
   render() {
     // console.log(this.props);
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className={"ui form error"}>
-        <Field name={"title"} component={this.renderInput} label="Enter Title"/>
-        <Field name={"description"} component={this.renderInput} label="Enter Description"/>
-        <button className={"ui button primary"}>Submit</button>
-      </form>
+      <div>
+        <h3> Create a Stream </h3>
+        <StreamForm onSubmit={this.onSubmit}/>
+      </div>
+      /* <form onSubmit={this.props.handleSubmit(this.onSubmit)} className={"ui form error"}>
+         <Field name={"title"} component={this.renderInput} label="Enter Title"/>
+         <Field name={"description"} component={this.renderInput} label="Enter Description"/>
+         <button className={"ui button primary"}>Submit</button>
+       </form>*/
     );
   }
 }
 
-const validate = formValues => {
+/*const validate = formValues => {
   const errors = {};
 
   if (!formValues.title) {
@@ -62,15 +66,15 @@ const validate = formValues => {
     errors.description = "You must enter a description";
   }
   return errors;
-};
+};*/
 
 
-const formWrapped = reduxForm({
+/*const formWrapped = reduxForm({
   form: "streamCreateTemp",
   validate //:validate  can be condensed since same name
-})(StreamCreate);
+})(StreamCreate);*/
 
-export default connect(null, {createStream})(formWrapped);
+export default connect(null, { createStream })(StreamCreate);  //(formWrapped);
 
 /*
 import React from "react";
