1f22240ffe4515d9fe032d9ff93ffddccfb08bfd
Rodrigo Chavez <rodctech@gmail.com>
2019-05-24 13:14:00 -0500

    L261   -Added URL-Based Nav to Edit Button

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -12,9 +12,12 @@ class StreamList extends Component {
       if (stream.userId === this.props.currentUserId ){
         return (
           <div className={"right floated content "}>
-            <button className={"ui button primary"}>
+           {/* <button className={"ui button primary"}>
               Edit
-            </button>
+            </button>*/}
+            <Link to={`/streams/edit/${stream.id}`} className={"ui button primary"}>
+            Edit
+            </Link>
             <button className={"ui button negative"}>
               Delete
             </button>
