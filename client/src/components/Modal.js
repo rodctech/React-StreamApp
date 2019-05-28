316f1ac93233e6e1eda38138d11474bbad0f28a2
Rodrigo Chavez <rodctech@gmail.com>
2019-05-28 12:24:48 -0500

    L274 -Edits to Modal + buttons  - Visual edits plus hiding when user clicks programmatic nav back to main page.  -Also if user clicks on modal window added .stopPropagation

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,15 +1,25 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import React from "react";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-      <div className={"ui dimmer modals visible active"}>
-        <div className={"ui standard modal visible active "}>
-          Modal blab
+    <div
+      onClick={()=> history.push('/')}
+      className={"ui dimmer modals visible active"}
+    >
+      <div onClick={(e) => e.stopPropagation()} className={"ui standard modal visible active "}>
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          <p>Are you sure you want to delete stream?</p>
         </div>
-      </div>,
-      document.querySelector('#modal')
-
+        <div className="actions">
+          <div className="ui primary button">Delete!</div>
+          <div className="ui cancel button">Cancel</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
   );
 };
 
