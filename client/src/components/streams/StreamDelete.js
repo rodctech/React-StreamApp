bdc60db2acfe02b197d6a04f779206af5004af43
Rodrigo Chavez <rodctech@gmail.com>
2019-05-28 13:38:14 -0500

    L280 -Added mapStateToProps, if ( renderContent )

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -22,20 +22,41 @@ class StreamDelete extends React.Component {
     );
   }
 
+  renderContent() {
+    if(!this.props.stream) {
+      return 'Are you sure you want to delete this stream?'
+    }
+    return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`
+  }
+
+
   render() {
+    //ONE Way to solve wait while compDidMount / we have the stream
+    /*if(!this.props.stream) {
+      return <div>Loading...</div>;
+    }*/
+
     return (
-      <div>
-        Stream Delete
+     // <div>
+     //   Stream Delete
         <Modal
           title={"Delete Stream"}
-          content={"Are you sure you want to delete stream?"}
+          content={this.renderContent()}
           actions={this.renderActions()}  //Make sure to call () method
           // to pass result to actions prop
           onDismiss={() => history.push("/")}
         />
-      </div>
+     // </div>
     );
   }
 }
 
-export default connect(null, {fetchStream}) (StreamDelete);
+const mapStateToProps = (state, ownProps) => {
+  //console.log(ownProps);
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+//state.streams is an object with all the streams we've loaded up.
+//you can flip back to reduxDevTools click on state button
+// and then expand streams. You find Object where keys are the IDs of streams
+
+export default connect(mapStateToProps, {fetchStream}) (StreamDelete);
