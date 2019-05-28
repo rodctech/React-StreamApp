6fe2adc698ae0c00408a04042749d0be9bf59f54
Rodrigo Chavez <rodctech@gmail.com>
2019-05-28 13:04:44 -0500

    L278 -added :id to URL Link for Delete button

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -18,13 +18,12 @@ class StreamList extends Component {
             <Link to={`/streams/edit/${stream.id}`} className={"ui button primary"}>
             Edit
             </Link>
-            <button className={"ui button negative"}>
+            <Link to={`/streams/delete/${stream.id}`} className={"ui button negative"}>
               Delete
-            </button>
+            </Link>
           </div>
         );
       }
-
     }
 
     renderList() {
